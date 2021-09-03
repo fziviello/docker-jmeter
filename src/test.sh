96c5fce97303032dd6ffaf802aec2477b2534e38
Fabio Ziviello <f.ziviello@email.it>
2021-09-03 14:43:25 +0200

    test

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+pwd
+ls -a
 REPORT_JTL=result.jtl
 REPORT_CSV=result.csv
 IFS=','
-[ ! -f $REPORT_JTL ] && { echo "$REPORT_JTL file not found"; exit 99; }
+[ ! -f $REPORT_JTL ] && { echo "$REPORT_JTL file not found"; exit -1; }
 sed -r '/^\s*$/d' < $REPORT_JTL > $REPORT_CSV
 while read timeStamp elapsed label responseCode responseMessage threadName dataType success failureMessage bytes sentBytes grpThreads allThreads URL Latency IdleTime Connect
 do
@@ -13,7 +15,7 @@ do
         then
              echo "$label" : "$success"
         else
-             exit -1
+             exit -9
         fi
     fi
 done < $REPORT_CSV
